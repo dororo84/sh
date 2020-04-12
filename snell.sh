55c39f01496151688b74ac2f900f9866ab566b8d
dororo84 <zeiss84@gmail.com>
2020-04-12 21:43:09 +0800

    Update snell.sh

--- snell.sh
+++ snell.sh
@@ -34,7 +34,7 @@ fi
 Download_snell(){
 	mkdir /root/snell
 	cd /root/snell
-    wget -N --no-check-certificate https://github.com/surge-networks/snell/releases/download/2.0.0/snell-server-v2.0.0-linux-amd64.zip
+    wget -N --no-check-certificate https://github.com/surge-networks/snell/releases/download/v2.0.1/snell-server-v2.0.1-linux-amd64.zip
     unzip snell*.zip
     mv snell-server snell
     rm -rf snell*.zip
